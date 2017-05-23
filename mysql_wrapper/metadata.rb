fae603ac0c2ccbb9fd09dd50713f1a7dccfb5b16
Pangs <pangs@Pangss-MacBook-Pro.local>
2017-05-23 20:54:10 +0530

    adding mysql recipes

--- mysql_wrapper/metadata.rb
+++ mysql_wrapper/metadata.rb
@@ -1,1 +1,8 @@
+name 'opsworks_mysql'
+maintainer       'binoy'
+maintainer_email 'binoy@gmail.com'
+license          'all_rights'
+description      'mysql'
+long_description 'mysql'
+version          '0.1.0'
 depends 'mysql', '~> 8.0'
