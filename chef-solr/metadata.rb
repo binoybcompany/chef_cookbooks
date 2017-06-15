72384072d35355fa1ef2fb49175198e22a70d5e0
Binoy <binoybcompany@users.noreply.github.com>
2017-06-16 00:57:42 +0530

    adding chef-colr

---
+++ chef-solr/metadata.rb
@@ -0,0 +1,21 @@
+name             'solr'
+maintainer       'David Radcliffe'
+maintainer_email 'radcliffe.david@gmail.com'
+license          'MIT'
+description      'Installs the solr search engine.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.6.0'
+
+supports 'amazon'
+supports 'centos'
+supports 'debian'
+supports 'redhat'
+supports 'scientific'
+supports 'ubuntu'
+
+depends 'apt'
+depends 'ark'
+depends 'java'
+
+source_url 'https://github.com/dwradcliffe/chef-solr' if respond_to?(:source_url)
+issues_url 'https://github.com/dwradcliffe/chef-solr/issues' if respond_to?(:issues_url)
